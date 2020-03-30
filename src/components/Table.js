fcd25ec9ee61f36c5dc4b8ca1c93c79ebcb5a725
Git Sadiq <abdulaisadiq@live.com>
2020-03-29 18:26:03 -0700

    Added table rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-export default function(props){
+export default function (props) {
     console.log(props.data)
-return(<>
-{props.data.map(person=> <div>{person.name.title} {person.name.first} {person.name.last}</div>)}
+    return (<>
+        {props.data.map(person =>
 
-</>)}
+
+            <div class="list-group list-group-item list-group-item-action list-group-item-secondary" id="list">
+
+                
+                    <img className="d-flex justify-content-start align-items-center" src={person.picture.thumbnail} />
+                    {person.name.title} 
+                    {person.name.first} 
+                    {person.name.last} 
+                    {person.phone}{person.email}
+                    <div className="d-flex justify-content-end align-items-center">{person.dob.age}</div>
+
+            </div>
+
+
+        )}
+
+
+    </>)
+}
